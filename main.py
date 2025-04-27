59b976ab2f9796f590f8540ee1793e58b7e24659
Gian <Gianrank87@gmail.com>
2025-04-27 20:54:37 -0300

    solucion de error en recursividad y agregue upper en el diccionario

--- main.py
+++ main.py
@@ -3,15 +3,16 @@
 import romano_a_decimal
 
 number = input("Ingrese un numero romano: ")
-print("El numero en decimal es: " + romano_a_decimal(number))
+print(f"El numero en decimal es: {romano_a_decimal.romano_a_decimal(number)}")
 
 # LA MOCHILA DEL JEDI #
 
 import usar_la_fuerza
 
+#defino la mochila con los siguientes objetos:
 mochila = ["comida", "botiquín", "cuerda", "sable de luz", "agua"]
 
-encontrado, cantidad = usar_la_fuerza(mochila)
+encontrado, cantidad = usar_la_fuerza.usar_la_fuerza(mochila)
 
 if encontrado:
     print(f"¡Sable encontrado! Se sacaron {cantidad} objetos.")
--- romano_a_decimal.py
+++ romano_a_decimal.py
@@ -1,23 +1,23 @@
+valores = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+}
+
 def romano_a_decimal(romano):
-      valores = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-      }
+    romano = romano.upper()
 
-      total = 0
-      anterior = 0
+    if len(romano) == 0:
+        return 0
 
-      for letra in reversed(romano.upper()):
-            valor = valores[letra]
-            if valor < anterior:
-                  total -= valor
-            else:
-                  total += valor
-            anterior = valor
+    if len(romano) == 1:
+        return valores[romano]
 
-      return total
+    if valores[romano[0]] >= valores[romano[1]]:
+        return valores[romano[0]] + romano_a_decimal(romano[1:])
+    else:
+        return -valores[romano[0]] + romano_a_decimal(romano[1:])
